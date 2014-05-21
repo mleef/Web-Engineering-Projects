8c23a96a9434e3749ecc960ab93d1c8de333871f
Marc Leef <mleef@uoregon.edu>
2014-05-21 11:43:16 -0700

    Started final project

---
+++ Final Project/models.js
@@ -0,0 +1,54 @@
+var UserSchema = { name : {type: String, unique: true}, 
+					undergrad : {school : String, major : String, minor : String, GPA : Number, extra : [String]},
+					GRE : {verbal : Number, quantitative : Number, writing : Number}, 
+					notes : String,
+					schools : [{school : String, program : String, accepted : Boolean}]
+				};
+
+var UserModel = { name : null, 
+					undergrad : null,
+					GRE : null,
+					notes : null,
+					schools : null,
+					get : function( field ){
+						if(this[field] === undefined ) {console.error("Field error" + field); return;}
+						return this[field];},
+					set : function( field, value ){
+						if(this[field] === undefined ) {console.error("Field error" + field); return;}
+						this[field] = value;
+					},
+					append : function( field, value ){
+						if(this[field] === undefined ) {console.error("Field error" + field); return;}
+						this[field].push(value);
+					},
+					getAll: function() {
+						var all_keys = Object.keys( this );
+						var model = this;
+						return all_keys.reduce( function (obj, current) {
+							if( model[current] typeof 'function') { return obj;}
+							obj[current] = model[current];
+							return obj;
+
+						}, {});
+					},
+					setAll: function ( obj ) {
+						var new_keys = Object.keys( obj );
+						var model = this;
+						new_keys.every ( function ( current ) {
+							if( model[current] === undefined ) {
+								console.error("Undefined key");
+								return true;
+							}
+							model[current] = obj[current];
+							return true;
+						})
+					} 
+				};
+
+
+
+// if( sessionStorage.getItem("name" === undefined)) sessionStorage.setItem("name", "foobar");
+// UserModel.set( name: sessionStorage.getItem( "name" ));
+// UserModel.set( history , ["diabetes"]);
+// UserModel.setAll({max_monthly: 200, current_providor: []})
+
